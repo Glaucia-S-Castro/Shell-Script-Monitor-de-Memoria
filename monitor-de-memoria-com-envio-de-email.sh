a7bc65d67c3d3845bea912feee31668ca450cb69
Glaucia Castro <glauciacastro.dev@gmail.com>
2024-07-12 22:08:43 -0300

    correction in file

--- monitor-de-memoria-com-envio-de-email.sh
+++ monitor-de-memoria-com-envio-de-email.sh
@@ -8,7 +8,7 @@ relacao_memoria_formatado=${relacao_memoria_atual_total}${simbolo}
 
 if [ $relacao_memoria_atual_total -gt 80 ]
 then
-	mail -a "From: email-rementente@outlook.com" -s "Consumo de memória acima do limite" glauciacastro.dev@gmail.com<<del
+	mail -a "From: email-rementente@email.com" -s "Consumo de memória acima do limite" email-destinatario@email.com<<del
 O consumo de memoria esta acima do limite que foi especificado. Atualmente o consumo e de $(free -h | grep -i mem | awk '{ print $3 }') que representa $relacao_memoria_formatado do total.
 del
 
